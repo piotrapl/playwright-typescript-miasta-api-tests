63643244aac42e2208136aad5b4591002f8cd03f
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-01-26 20:25:29 +0100

    dodawanie opisów

--- tests/tests/data/municipalitiesByName.cases.ts
+++ tests/tests/data/municipalitiesByName.cases.ts
@@ -1,3 +1,9 @@
+// tests/tests/data/municipalitiesByName.cases.ts
+// przypadki testowe - wyszukiwanie miast po nazwie
+// eksportujemy tablicę - zawiera obiekty z nazwami miast
+// nowe przypadki można dodawać do tej tablicy
+// 'as const' - ma zapewnić niezmienność tablicy
+
 export const municipalitiesByNameCases = [
   { name: "Lodz" },
   { name: "Lublin" }
